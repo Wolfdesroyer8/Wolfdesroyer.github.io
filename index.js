00af0c6406a85e1f3a222ef7d363b69f7fba7c60
Wolfdesroyer8 <75499941+Wolfdesroyer8@users.noreply.github.com>
2021-03-25 15:35:57 -0700

    catching up

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+console.log("JS loaded");
+function but1() {
+    scroll(1200);
+    console.log("about clicked");
+};
 
+function but2() {
+    scroll(2200);
+    console.log("projects clicked");
+};
+
+function but3() {
+    scroll(3150);
+    console.log("contact clicked");
+};
+
+function scroll(px) {
+    for (var i=0; i<px; i ++) {
+        setTimeout(window.scrollBy(0, i), 500);
+    };
+};
